8fb225fcfdfdd50a780b4a0096313d7911985333
nisitomy <nisi.thomas@outlook.fr>
2022-12-12 16:52:21 +0100

    ininiii

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\EmailVerifier;
@@ -27,7 +28,7 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        $user = new User();
+        $user = new Student();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
